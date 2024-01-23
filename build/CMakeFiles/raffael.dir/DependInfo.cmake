f7de36f0db5f5f6afda4f00dbb2c263250a3dffe
re23m026 <re23m026@technikum-wien.at>
2024-01-23 21:41:27 +0100

    Finale Version

---
+++ build/CMakeFiles/raffael.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/parallels/Desktop/raffael/src/CommandServer.cpp" "CMakeFiles/raffael.dir/src/CommandServer.cpp.o" "gcc" "CMakeFiles/raffael.dir/src/CommandServer.cpp.o.d"
+  "/home/parallels/Desktop/raffael/src/CommandServer_Alt.cpp" "CMakeFiles/raffael.dir/src/CommandServer_Alt.cpp.o" "gcc" "CMakeFiles/raffael.dir/src/CommandServer_Alt.cpp.o.d"
+  "/home/parallels/Desktop/raffael/src/DieWithError.cpp" "CMakeFiles/raffael.dir/src/DieWithError.cpp.o" "gcc" "CMakeFiles/raffael.dir/src/DieWithError.cpp.o.d"
+  "/home/parallels/Desktop/raffael/src/Final_CommandServer.cpp" "CMakeFiles/raffael.dir/src/Final_CommandServer.cpp.o" "gcc" "CMakeFiles/raffael.dir/src/Final_CommandServer.cpp.o.d"
+  "/home/parallels/Desktop/raffael/src/Final_OdomClient.cpp" "CMakeFiles/raffael.dir/src/Final_OdomClient.cpp.o" "gcc" "CMakeFiles/raffael.dir/src/Final_OdomClient.cpp.o.d"
+  "/home/parallels/Desktop/raffael/src/Final_RangesClient.cpp" "CMakeFiles/raffael.dir/src/Final_RangesClient.cpp.o" "gcc" "CMakeFiles/raffael.dir/src/Final_RangesClient.cpp.o.d"
+  "/home/parallels/Desktop/raffael/src/OdomClient.cpp" "CMakeFiles/raffael.dir/src/OdomClient.cpp.o" "gcc" "CMakeFiles/raffael.dir/src/OdomClient.cpp.o.d"
+  "/home/parallels/Desktop/raffael/src/RangesClient.cpp" "CMakeFiles/raffael.dir/src/RangesClient.cpp.o" "gcc" "CMakeFiles/raffael.dir/src/RangesClient.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/raffael.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raffael.dir/src/CommandServer.cpp.o"
+  "CMakeFiles/raffael.dir/src/CommandServer.cpp.o.d"
+  "CMakeFiles/raffael.dir/src/CommandServer_Alt.cpp.o"
+  "CMakeFiles/raffael.dir/src/CommandServer_Alt.cpp.o.d"
+  "CMakeFiles/raffael.dir/src/DieWithError.cpp.o"
+  "CMakeFiles/raffael.dir/src/DieWithError.cpp.o.d"
+  "CMakeFiles/raffael.dir/src/Final_CommandServer.cpp.o"
+  "CMakeFiles/raffael.dir/src/Final_CommandServer.cpp.o.d"
+  "CMakeFiles/raffael.dir/src/Final_OdomClient.cpp.o"
+  "CMakeFiles/raffael.dir/src/Final_OdomClient.cpp.o.d"
+  "CMakeFiles/raffael.dir/src/Final_RangesClient.cpp.o"
+  "CMakeFiles/raffael.dir/src/Final_RangesClient.cpp.o.d"
+  "CMakeFiles/raffael.dir/src/OdomClient.cpp.o"
+  "CMakeFiles/raffael.dir/src/OdomClient.cpp.o.d"
+  "CMakeFiles/raffael.dir/src/RangesClient.cpp.o"
+  "CMakeFiles/raffael.dir/src/RangesClient.cpp.o.d"
+  "raffael"
+  "raffael.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raffael.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
